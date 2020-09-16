ffa8fb0ba3d99bc5c941c2e872873f8f5a9b9397
avirallakhanpaul <avirallakhanpaul8@gmail.com>
2020-09-16 18:59:56 +0530

    Added TargetMover Script and Target Prefab

---
+++ Assets/TargetMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMove : MonoBehaviour {
+
+	public enum motionDirection { Spin, Horizontal, Vertical };
+	public motionDirection motionState = motionDirection.Horizontal;
+
+	public float spinSpeed = 180.0f;
+	public float motionValue = 0.1f;
+
+    void Update() {
+
+		switch(motionState) {
+
+			case motionDirection.Spin:
+				gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+				break;
+
+			case motionDirection.Horizontal:
+				gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionValue);
+				break;
+
+			case motionDirection.Vertical:
+				gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionValue);
+				break;
+		}
+    }
+}
