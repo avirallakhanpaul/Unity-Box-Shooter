50654df9a3042ec9094b8f78a0b40b1094f4d5e3
avirallakhanpaul <avirallakhanpaul8@gmail.com>
2020-09-26 17:41:34 +0530

    Completed Project

---
+++ Assets/Scripts/TriggerLevelLoad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TriggerLevelLoad : MonoBehaviour {
+
+	public string nameOfLevelToLoad  = "";
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Projectile" )
+		{
+			SceneManager.LoadScene(nameOfLevelToLoad);
+		}
+	}
+}
